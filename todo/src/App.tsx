77bbbbb52eb2c663462c4fe9910071fcf816dd1b
AminSalkovic <aminsalkovic2019@gmail.com>
2022-10-26 20:36:38 +0200

    todo

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,5 +1,6 @@
 import React,{ChangeEvent, FC,useState} from 'react';
 import './App.css';
+import TodoTask from './components/TodoTask';
 import{ITask} from './interfaces/Interfaces'
 
 const App: FC =()=> {
@@ -11,8 +12,6 @@ const App: FC =()=> {
   const handleChange=(event: ChangeEvent<HTMLInputElement>):void=>{
     if(event.target.value==='task'){
       setTask(event.target.value)
-      console.log(task);
-      
     }else{
       setDeadline(Number(event.target.value))
     }
@@ -20,19 +19,37 @@ const App: FC =()=> {
   const addTask=():void=>{
       const newTask={taskName:task,deadline:deadline}
         setTodo([...todo,newTask])
+        setTask('')
+        setDeadline(0)
         console.log(todo);
-        
+  }
+  const completeTask=(taskNameToDelete:string):void=>{
+     setTodo(todo.filter((task)=>{
+      return task.taskName = taskNameToDelete
+     }))
   }
   return (
     <div className="App">
        <div className="header">
         <div className="inputContainer">
-        <input type="text" placeholder='task...' name='task' onChange={handleChange}/>
-        <input type="number" placeholder='Deadline..' name='deadline' />
+        <input type="text" placeholder='task...' 
+         value={task}
+        name='task' onChange={handleChange}/>
+        <input type="number" placeholder='Deadline..' 
+          name='deadline' 
+          value={deadline}
+        />
         </div>
         <button onClick={addTask}>add task</button>
        </div>
-       <div className="todoList"></div>
+       <div className="todoList">
+        
+        {todo.map((task:ITask,key:number)=>{
+             return <TodoTask key={key} 
+             completeTask={completeTask}
+             task={task}/>
+          })}
+       </div>
     </div>
   );
 }
